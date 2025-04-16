9f04bc3ee202773a6159b289c2ea47ed7a20f2e4
Ananya Singh <singhananya5040@gmail.com>
2025-04-16 11:08:32 +0530

    16-04-25

--- controllers/url.js
+++ controllers/url.js
@@ -1,16 +1,34 @@
-const {nanoid}= require('nanoid');
+const { nanoid } = require("nanoid");
 const URL = require("../models/url");
 
-async function handleShortURL(req,res){
-    const body = req.body;
-    if(!body.url) return res.status(400).json({error: "URL is required!"});
-    const shortID = nanoid(8);
-    await URL.create({
-        shortId: shortID,
-        redirectUrl: body.url,
-
-    })
-    return res.json({id: shortID});
-
+async function handleShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "URL is required!" });
+  const shortID = nanoid(8);
+  await URL.create({
+    shortId: shortID,
+    redirectUrl: body.url,
+  });
+  return res.json({ id: shortID });
 }
-module.exports= {handleShortURL};
+async function handleRedirect(req, res) {
+  const shortID = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId: shortID,
+    },
+    {
+        // for logging the time.
+      $push: {
+        visitHistory: {
+          timestamps: Date.now(),
+        },
+      },
+    }
+  );
+  if (!entry) {
+    return res.status(404).send("Short URL not found");
+  }
+  res.redirect(entry.redirectUrl);
+}
+module.exports = { handleShortURL, handleRedirect };
