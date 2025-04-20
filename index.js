92154109ebcb801c3e9a30d8c1362394941afdbc
Ananya Singh <singhananya5040@gmail.com>
2025-04-20 21:38:02 +0530

    20-04-25

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const router = require("./routes/url.js");
 const staticRoute = require("./staticRouter.js");
 const userRoute = require("./routes/user.js");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const URL = require("./models/url.js");
 const { connectDb } = require("./connect.js");
+const {
+  restrictToLoggedInUserOnly,
+  checkAuth,
+} = require("./middlewares/auth.js");
 const app = express();
 const PORT = 8001;
 //Connection:
@@ -19,15 +24,13 @@ app.set("views", path.resolve("./views"));
 //Middleware:
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); //used to parse cookies.
 
-//Routes:
-app.use("/", staticRoute);
-app.use("/url", router);
-app.use("/user",userRoute);
+app.use("/", checkAuth, staticRoute);
+app.use("/url", restrictToLoggedInUserOnly, router); //If you wish to access anything starting with url route, you need to be logged in (Inline middleware)
+app.use("/user", userRoute);
 
-
-
-
+// LOGIC FOR REDIRECTION:
 app.get("/:shortId", async (req, res) => {
   const shortID = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
