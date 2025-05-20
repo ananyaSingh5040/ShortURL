a619bb8a1e771376094737d66f06371dc309ba06
Ananya Singh <singhananya5040@gmail.com>
2025-05-20 09:08:21 +0530

    20-05-25

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,10 @@ const { v4: uuidv4 } = require("uuid");
 const { setUser } = require("../service/auth");
 const User = require("../models/user");
 async function handleUserSignUp(req, res) {
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (existingUser) {
+    return res.status(400).send("User already exists");
+  }
   const { name, email, password } = req.body;
   await User.create({
     name,
@@ -11,7 +15,7 @@ async function handleUserSignUp(req, res) {
   return res.render("home");
 }
 async function handleUserLogin(req, res) {
-  const {email, password } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email, password });
   if (!user)
     return res.render("login", {
